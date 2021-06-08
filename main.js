aadd3fa1246a452bc7e149de72e6c665b87ec50e
ishaongithub <75757332+ishaongithub@users.noreply.github.com>
2021-06-08 14:04:14 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -16,4 +16,23 @@ recognition.onresult = function(event) {
 
 var Content = event.results[0][0].transcript;
     document.getElementById("textbox").innHTML= Content;
+    console.log(Content);
+    speak();
 }
+
+function speak(){
+    var synth= window.speechSynthesis;
+    speak_data= document.getElementById("textbox").value;
+    var utterThis= new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width:260,
+    height:250,
+    image_format:"png",
+    png_quality:90
+
+});
+camera = document.getElementById("camera");
