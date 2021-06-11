669bbfa4cb8e75f60b71436f20528aad9b4a0f7e
ishaongithub <75757332+ishaongithub@users.noreply.github.com>
2021-06-11 16:18:11 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -17,22 +17,43 @@ recognition.onresult = function(event) {
 var Content = event.results[0][0].transcript;
     document.getElementById("textbox").innHTML= Content;
     console.log(Content);
-    speak();
+    if (Content== "take my selfie"){
+        console.log("taking selfie ---");
+        speak();
+
+    }
+    
 }
 
 function speak(){
     var synth= window.speechSynthesis;
-    speak_data= document.getElementById("textbox").value;
+    speak_data= "Taking your Selfie in 5 seconds";
     var utterThis= new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000);
 }
 
 Webcam.set({
-    width:260,
+    width:360,
     height:250,
     image_format:"png",
     png_quality:90
 
 });
 camera = document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML='<img id="selfie_img" src="'+data_uri+'">';
+    });
+}
+function save(){
+    link=document.getElementById("link");
+    image= document.getElementById("selfie_img").src;
+link.href=image;
+link.click();
+}
